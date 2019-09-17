54093d2ee716da5e8b7ee4de56ad118b642dc4ac
Tyler Vultaggio <tylervultaggio101@gmail.com>
2019-09-16 20:48:37 -0400

    Solution to Madlib Program.

---
+++ madlib.py
@@ -0,0 +1,14 @@
+#Introduction to Programming
+#Author: Tyler Vultaggio
+#Date: 9/16/2019
+
+def main():
+
+    noun = input("Enter a Noun: ")
+    verb = input("Enter a Verb: ")
+    adjective = input("Enter a Adjective: ")
+    place = input("Enter a Place: ")
+
+    print("When I go to", place, "I", adjective, "like a", verb, "then", noun, "pulls up!")
+
+main()
