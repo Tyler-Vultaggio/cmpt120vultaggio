ed41cff4962bc3fb61f99aa0a9eee828a6eae76b
Tyler Vultaggio <tylervultaggio101@gmail.com>
2019-10-01 16:14:01 -0400

    Second Part of Working with Strings lab 4.

--- Working with Strings.py
+++ Working with Strings.py
@@ -3,21 +3,34 @@
 #Author: Tyler Vultaggio
 #Date: 10/1/2019
 
-
-def main():
+def userInput():
     # get user's first and last names
     first = input("Enter your first name: ")
     last = input("Enter your last name: ")
-    # TODO modify this to generate a Marist-style username
-    uname = first + "." + last
+    firstLast = [first, last]
+    return firstLast
+
+def userName(firstLast):
+    uname = firstLast[0] + "." + firstLast[1]
+    return uname
+
+def passWord():
     # ask user to create a new password
     passwd = input("Create a new password: ")
-    # TODO modify this to ensure the password has at least 8 characters
     if len(passwd) < 8:
         while len(passwd) < 8:
             print("Fool of a Took! That password is feeble!")
             passwd = input("Create a new password: ")
         print("The force is strong in this one…")
+    return passwd
+
+
+def main():
+    firstLast = userInput()
+    uname = userName(firstLast)
+    password = passWord()
+    print(password)
+    # TODO modify this to ensure the password has at least 8 characters
     print("Account configured. Your new email address is", uname + "1@marist.edu")
     
 main()
